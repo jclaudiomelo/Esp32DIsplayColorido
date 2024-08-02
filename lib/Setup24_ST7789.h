c27d30bc222c761a998797eae67fa991f8dcd451
jclaudiomelo <claudiojcmn@yahoo.com.br>
2024-08-02 18:26:55 -0300

    refatore

--- lib/Setup24_ST7789.h
+++ lib/Setup24_ST7789.h
@@ -11,6 +11,27 @@
 //#define TFT_INVERSION_ON
 //#define TFT_INVERSION_OFF
 
+// // Definindo as cores
+#define TFT_BLACK       0x0000      /*   0,   0,   0 */
+#define TFT_NAVY        0x000F      /*   0,   0, 128 */
+#define TFT_DARKGREEN   0x03E0      /*   0, 128,   0 */
+#define TFT_DARKCYAN    0x03EF      /*   0, 128, 128 */
+#define TFT_MAROON      0x7800      /* 128,   0,   0 */
+#define TFT_PURPLE      0x780F      /* 128,   0, 128 */
+#define TFT_OLIVE       0x7BE0      /* 128, 128,   0 */
+#define TFT_LIGHTGREY   0xD69A      /* 211, 211, 211 */
+#define TFT_DARKGREY    0x7BEF      /* 128, 128, 128 */
+#define TFT_BLUE        0x001F      /*   0,   0, 255 */
+#define TFT_GREEN       0x07E0      /*   0, 255,   0 */
+#define TFT_CYAN        0x07FF      /*   0, 255, 255 */
+#define TFT_RED         0xF800      /* 255,   0,   0 */
+#define TFT_MAGENTA     0xF81F      /* 255,   0, 255 */
+#define TFT_YELLOW      0xFFE0      /* 255, 255,   0 */
+#define TFT_WHITE       0xFFFF      /* 255, 255, 255 */
+#define TFT_ORANGE      0xFD20
+#define TFT_GREENYELLOW 0xAFE5
+#define TFT_PINK        0xF81F
+
 // Configuração DSTIKE stepup
 #define TFT_DC    23
 #define TFT_RST   32
--- src/main.cpp
+++ src/main.cpp
@@ -1,43 +1,19 @@
 #include <SPI.h>
 #include <TFT_eSPI.h>  // Biblioteca específica do hardware
 
-#define SCREEN_WIDTH 240
-#define SCREEN_HEIGHT 240
-
-// Definindo as cores
-#define TFT_BLACK       0x0000      /*   0,   0,   0 */
-#define TFT_NAVY        0x000F      /*   0,   0, 128 */
-#define TFT_DARKGREEN   0x03E0      /*   0, 128,   0 */
-#define TFT_DARKCYAN    0x03EF      /*   0, 128, 128 */
-#define TFT_MAROON      0x7800      /* 128,   0,   0 */
-#define TFT_PURPLE      0x780F      /* 128,   0, 128 */
-#define TFT_OLIVE       0x7BE0      /* 128, 128,   0 */
-#define TFT_LIGHTGREY   0xD69A      /* 211, 211, 211 */
-#define TFT_DARKGREY    0x7BEF      /* 128, 128, 128 */
-#define TFT_BLUE        0x001F      /*   0,   0, 255 */
-#define TFT_GREEN       0x07E0      /*   0, 255,   0 */
-#define TFT_CYAN        0x07FF      /*   0, 255, 255 */
-#define TFT_RED         0xF800      /* 255,   0,   0 */
-#define TFT_MAGENTA     0xF81F      /* 255,   0, 255 */
-#define TFT_YELLOW      0xFFE0      /* 255, 255,   0 */
-#define TFT_WHITE       0xFFFF      /* 255, 255, 255 */
-
 TFT_eSPI tft = TFT_eSPI();  // Invoca a biblioteca personalizada
+
 
 void setup(void) {
   Serial.begin(9600);        // Inicializa a comunicação serial
   tft.init();                // Inicializa o display
-
   tft.setRotation(3); // Ajusta a rotação conforme necessário
-
   tft.invertDisplay(true);  // Ativa a inversão de cores
-
-  // Limpa a tela
-  tft.fillScreen(TFT_BLACK);
+  tft.fillScreen(TFT_BLACK); // Limpa a tela
 
   // Desenha retângulos e escreve o texto em cada cor
   int y = 0;
-  int colorHeight = SCREEN_HEIGHT / 16; // Divida a tela em 16 segmentos
+  int colorHeight = TFT_HEIGHT / 16; // Divida a tela em 16 segmentos
 
   // Array com as cores e os nomes
   struct ColorText {
@@ -65,7 +41,7 @@ void setup(void) {
   };
 
   for (int i = 0; i < 16; i++) {
-    tft.fillRect(0, y, SCREEN_WIDTH, colorHeight, colorTexts[i].color);
+    tft.fillRect(0, y, TFT_WIDTH, colorHeight, colorTexts[i].color);
     
     // Configura o cursor para o centro do retângulo
     tft.setCursor(10, y + colorHeight / 2 - 8, 2); // Ajuste a posição do cursor
